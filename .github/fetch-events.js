bebddf66d867eef826bae3d39c0e55ed2176086a
Sapnil Aditya Ayman <sapnilaa@ulrik.uio.no>
2026-01-22 14:02:06 +0100

    Compare eventId for redeployment instead of event date

--- .github/fetch-events.js
+++ .github/fetch-events.js
@@ -9,25 +9,37 @@ const params = new URLSearchParams({
 });
 
 const url = `${peoplyUrl}?${params}`;
-let event;
+const eventIdFile = "_data/eventId.txt";
 
-// Checks if a new event has occured in Peoply:
-const hasNewEvent = (event) => {
-  console.log("Fetched latest event:", event);
+let prevEventId = "";
 
-  const currentDate = new Date().toLocaleString("no-NB", { timeZone: "Europe/Oslo" });
-  const eventDate = new Date(event.startDate).toLocaleString("no-NB", { timeZone: "Europe/Oslo" });
-
-  console.log("Current: ", currentDate);
-  console.log("Event: ", eventDate);
-
-  return currentDate < eventDate;
+if (fs.existsSync(eventIdFile)) {
+  prevEventId = fs.readFileSync(eventIdFile, "utf-8").trim();
 };
 
 fetch(url)
   .then((res) => res.json())
   .then((data) => {
-    console.log("Fetched events:", data);
+    // console.log("Fetched events:", data);
+
+    if (!data || data.length === 0) {
+      console.log("No data returned from Peoply.")
+      process.exit(0);
+    };
+
+    // Accesss latest event ID
+    const latestEvent = data[0];
+    const latestEventId = latestEvent.id;
+
+    // Stop if no new event
+    if (latestEventId === prevEventId) {
+      console.log("No new events in Peoply.");
+      process.exit(0);
+    };
+
+    // New event found
+    console.log("New event added in Peoply.");
+    fs.writeFileSync(eventIdFile, latestEventId);
 
     // Adjust event time to follow timezone in Norway:
     const updatedData = data.map(event => {
@@ -38,12 +50,9 @@ fetch(url)
     })
 
     fs.writeFileSync("_data/events.json", JSON.stringify(updatedData, 2));
-    console.log("Events written to _data/events.json");
+    // console.log("Events written to _data/events.json");
 
-    // Get first event from Peoply as events are ordered by date
-    const values = Object.values(updatedData);
-    event = values[0];
-    hasNewEvent(event);
+    process.exit(1);
   })
   .catch((err) => {
     console.error("Failed to fetch events:", err);
